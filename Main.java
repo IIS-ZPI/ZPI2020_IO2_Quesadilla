9cf56bc8b875127afc072e3f47a5c194c5350969
Oskar Mikus <oskarmikus@protonmail.com>
2021-03-16 16:49:37 +0100

    Added OM info

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Quesadilla https://github.com/TinyRogue");
+    System.out.println("https://github.com/TinyRogue");
   }
 }
