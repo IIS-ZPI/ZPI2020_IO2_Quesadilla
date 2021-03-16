4dc5139c122ece4410c73495a228778b5a1eb71e
LogicalInsanity <idkwtw223@gmail.com>
2021-03-16 16:54:25 +0100

    info added

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Quesadilla https://github.com/TinyRogue");
+    System.out.println("LogicalInsanity https://github.com/LogicalInsanity");
   }
 }
