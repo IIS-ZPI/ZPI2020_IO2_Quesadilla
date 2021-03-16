0fd6d7a4a3af18244417658ca47606a432917b0e
LogicalInsanity <idkwtw223@gmail.com>
2021-03-16 17:40:39 +0100

    #2

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
+    //really? maybe do 3 next time?
     System.out.println("Quesadilla https://github.com/TinyRogue");
     System.out.println("Oskar Mikus https://github.com/TinyRogue");
     System.out.println("Kateryna Ocheretian https://github.com/cheretka");
