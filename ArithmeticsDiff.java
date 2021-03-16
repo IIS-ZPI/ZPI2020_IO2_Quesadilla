67811e484e0b58133e6887ad53c036fc6a105a57
Oskar Mikus <oskarmikus@protonmail.com>
2021-03-16 17:42:09 +0100

    Tell me why

--- ArithmeticsDiff.java
+++ ArithmeticsDiff.java
@@ -1,5 +1,5 @@
 public class ArithmeticsDiff implements IArithmeticsDiff{
     public double difference(double A, double B){
-        return A-B;
+        return A-B; //Tell me why! No space is here na na na
     }
 }
