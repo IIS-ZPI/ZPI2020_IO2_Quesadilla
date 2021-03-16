490baa575772ee4ae2731d3043b5e0abfdd5c640
Katherine <ocheretianfws@gmail.com>
2021-03-16 17:56:47 +0200

    add line

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Quesadilla https://github.com/TinyRogue");
+    System.out.println("Kateryna Ocheretian https://github.com/cheretka");
   }
+
 }
