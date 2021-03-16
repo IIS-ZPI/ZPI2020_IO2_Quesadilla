a7e47eab1d10b1732710ae8403d0ad9e1a57b39e
LogicalInsanity <idkwtw223@gmail.com>
2021-03-16 17:43:40 +0100

    #4

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
+    //then we would have freedom to set it as whatever
     System.out.println("Quesadilla https://github.com/TinyRogue");
     System.out.println("Oskar Mikus https://github.com/TinyRogue");
     System.out.println("Kateryna Ocheretian https://github.com/cheretka");
