10085328d377b1b63b180a190d49bb01090b1912
Katherine <ocheretianfws@gmail.com>
2021-03-16 18:43:20 +0200

    cherry picked the finest function

---
+++ ArithmeticsDiff.java
@@ -0,0 +1,6 @@
+//a class that implements a function that computes the differences of two numbers
+public class ArithmeticsDiff implements IArithmeticsDiff{
+    public double difference(double A, double B){
+        return A-B;
+    }
+}
