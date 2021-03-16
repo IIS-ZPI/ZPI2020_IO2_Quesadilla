758229ed2b8f9e58154c22b8fbd8b0cda16c6190
Katherine <ocheretianfws@gmail.com>
2021-03-16 18:44:04 +0200

    added comment3

--- ArithmeticsDiff.java
+++ ArithmeticsDiff.java
@@ -1,4 +1,5 @@
 public class ArithmeticsDiff implements IArithmeticsDiff{
+    //function that calculates the difference of two numbers
     public double difference(double A, double B){
         return A-B;
     }
