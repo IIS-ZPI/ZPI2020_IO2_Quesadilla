9b1e435fd7a69dc2d1eabd5efd08c06440c39e36
Wiktor Kania <roller47@wp.pl>
2021-03-16 16:47:54 +0100

    added my own info

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Quesadilla https://github.com/TinyRogue");
+    System.out.println("Wiktor Kania https://github.com/Roller23");
   }
 }
