d864044804876b758b5f74b42968daa060801477
Katherine <ocheretianfws@gmail.com>
2021-03-30 17:31:43 +0300

    very important interface

---
+++ IArithmeticsDiff.java
@@ -0,0 +1,5 @@
+//interface responsible for representing the difference of two numbers
+public interface IArithmeticsDiff {
+    // count the difference of two numbers
+    double difference(double A, double B);
+}
