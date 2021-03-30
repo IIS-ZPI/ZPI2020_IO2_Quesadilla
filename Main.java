478fb23769ea0f352e6898f97bddff384be26b70
Wiktor Kania <roller47@wp.pl>
2021-03-30 16:29:37 +0200

    added a comment

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 class Main {
+  // THIS COMMENT WILL DISAPPEAR IN 24 HOURS CLICK TO GET -80% OFF-
   public static void main(String[] args) {
     // double space tabs supremacy
     System.out.println("Quesadilla https://github.com/TinyRogue");
