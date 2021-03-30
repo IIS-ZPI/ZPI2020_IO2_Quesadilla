de78ff2971bd407a6a02b4ce4425a806b66bdeb8
Wiktor Kania <roller47@wp.pl>
2021-03-30 17:34:08 +0200

    cherry picked only the finest commits

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  // THIS COMMENT WILL DISAPPEAR IN 24 HOURS CLICK TO GET -80% OFF-
+  // why 2 spaces?
+  public static void main(String[] args) {
+    //really? maybe do 3 next time?
+    //why not tabs, it would make life easier
+    //then we would have freedom to set it as whatever
+    // double space tabs supremacy
+    System.out.println("Quesadilla https://github.com/TinyRogue");
+    System.out.println("Oskar Mikus https://github.com/TinyRogue");
+    System.out.println("Kateryna Ocheretian https://github.com/cheretka");
+    System.out.println("LogicalInsanity https://github.com/LogicalInsanity");
+    //Don't trust him, he uses 2 spaces indent in Java
+    System.out.println("Wiktor Kania https://github.com/Roller23");
+  }
+  // this is the greatest feature that has ever been created
+  // it does absolutely nothing
+}
