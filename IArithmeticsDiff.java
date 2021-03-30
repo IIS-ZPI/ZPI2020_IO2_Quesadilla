2fb11215877fd951c339b4380d2bcd50a7e246e8
Cheret Katherine <63000502+cheretka@users.noreply.github.com>
2021-03-30 17:54:47 +0300

    Revert "changed 1 line"

--- IArithmeticsDiff.java
+++ IArithmeticsDiff.java
@@ -1,5 +1,4 @@
 //interface responsible for representing the difference of two numbers
 public interface IArithmeticsDiff {
-    // count the difference of two numbers
     double difference(double A, double B);
 }
