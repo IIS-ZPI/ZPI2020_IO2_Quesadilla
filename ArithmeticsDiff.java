7adeb97752759e5a6430078b7e9346d97a6ab4ef
Oskar Mikus <oskarmikus@protonmail.com>
2021-04-20 17:19:44 +0200

    Delete ArithmeticsDiff.java

--- ArithmeticsDiff.java
+++
@@ -1,7 +0,0 @@
-
-//a class that implements a function that computes the differences of two numbers
-public class ArithmeticsDiff implements IArithmeticsDiff{
-    public double difference(double A, double B){
-        return A-B;
-    }
-}
